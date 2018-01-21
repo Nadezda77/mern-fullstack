82aa99f33b615a86bb3037e6f08f1b7e970eaa4a
nada <nadin.vasic@gmail.com>
2018-01-21 15:28:35 +0000

    bez reg.serv.worker u index_2 i instaliran static server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import Edit from './components/Edit';
 import Create from './components/Create';
 import Show from './components/Show';
